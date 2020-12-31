91643457fd564def4fa8e19416335ae1937c09cc
khadijaSerag <khadija.serag.98@gmail.com>
2020-12-31 11:48:37 +0200

    inital

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function turnOffBtn(){
+     document.getElementById("imgGraph").src ="images/pic_bulboff.gif";
+     document.getElementById("bodyPage").style.backgroundColor="blueviolet";
+}
+function turnOnBtn(){
+    document.getElementById("imgGraph").src ="images/pic_bulbon.gif";
+    document.getElementById("bodyPage").style.backgroundColor="#18b4cc";
+}
